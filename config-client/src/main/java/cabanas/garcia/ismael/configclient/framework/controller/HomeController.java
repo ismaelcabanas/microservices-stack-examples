b15b829c42305631ebaa26724cd22dc81b3c77ff
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-06-07 12:21:11 +0200

    New controller method
    In this commit I have created a hot() method for showing how to use a bean
    configured with @RefreshScope annotation.

--- config-client/src/main/java/cabanas/garcia/ismael/configclient/framework/controller/HomeController.java
+++ config-client/src/main/java/cabanas/garcia/ismael/configclient/framework/controller/HomeController.java
@@ -1,5 +1,7 @@
 package cabanas.garcia.ismael.configclient.framework.controller;
 
+import cabanas.garcia.ismael.configclient.framework.service.HotMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,16 @@ public class HomeController {
     @Value("${message:World}")
     private String name;
 
+    @Autowired
+    private HotMessage hotMessage;
+
     @RequestMapping("/")
     public String home() {
         return String.format(template, name);
+    }
+
+    @RequestMapping(value = "/hot")
+    public String hot(){
+        return String.format(template, hotMessage.message());
     }
 }
